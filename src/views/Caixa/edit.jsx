e088dc713340643eafe76e58f6ede67f47cdc6bc
victorassisr <victorassis545@gmail.com>
2019-11-23 16:59:29 -0300

    Alterações em caixa

--- src/views/Caixa/edit.jsx
+++ src/views/Caixa/edit.jsx
@@ -181,7 +181,10 @@ class editBox extends React.Component {
       bairros: [],
       spliters: [],
       idBairro: "",
-      idSpliter: ""
+      idSpliter: "",
+      descricao: "",
+      latitude: "",
+      longitude: ""
     };
     this.handleChange = this.handleChange.bind(this);
     this.edit = this.edit.bind(this);
@@ -218,8 +221,18 @@ class editBox extends React.Component {
     event.preventDefault();
     const handle = this.props.match.params;
     this.getUserLogged();
-
-    //Editar caixa....
+    axios
+      .put(`${utils.URL_BASE_API}/cto/${handle.id}`, {
+        headers: {
+          "X-Access-Token": this.getUserLogged().token
+        }
+      })
+      .then(res => {
+        alert("Editado com sucesso!");
+      })
+      .catch(err => {
+        alert("Erro ao editar!");
+      });
   };
 
   //Metodo que pega o usuário da session;
@@ -240,8 +253,14 @@ class editBox extends React.Component {
       })
       .then(res => {
         this.setState({
-          caixa: res.data[0]
+          caixa: res.data[0],
+          latitude: res.data[0].latitude,
+          longitude: res.data[0].longitude,
+          descricao: res.data[0].descricao,
+          idBairro: res.data[0].bairro.idBairro,
+          idSpliter: res.data[0].spliter.idSpliter
         });
+        console.log(res.data[0].bairro);
       })
       .catch(err => {
         utils.showError(err);
@@ -299,23 +318,27 @@ class editBox extends React.Component {
       SingleValue
     };
 
-    const allBairros = this.state.bairros
-      .map(bairro => {
-        return { label: bairro.descricao, value: bairro.idBairro };
-      })
-      .map(bairros => ({
+    const allBairros = this.state.bairros.map(bairro => {
+      return {
+        label: bairro.descricao,
+        value: bairro.idBairro
+      };
+    });
+    /*.map(bairros => ({
         value: bairros.value,
         label: bairros.label
-      }));
+      }));*/
 
-    const allSpliters = this.state.spliters
-      .map(spliter => {
-        return { label: spliter.descricao, value: spliter.idSpliter };
-      })
-      .map(spliters => ({
+    const allSpliters = this.state.spliters.map(spliter => {
+      return {
+        label: spliter.descricao,
+        value: spliter.idSpliter
+      };
+    });
+    /*.map(spliters => ({
         value: spliters.value,
         label: spliters.label
-      }));
+      }));*/
 
     return (
       <div>
@@ -337,7 +360,7 @@ class editBox extends React.Component {
                         }}
                         inputProps={{
                           name: "latitude",
-                          value: this.state.caixa.latitude,
+                          value: this.state.latitude,
                           onChange: this.onChange,
                           required: true
                         }}
@@ -352,7 +375,7 @@ class editBox extends React.Component {
                         }}
                         inputProps={{
                           name: "longitude",
-                          value: this.state.caixa.longitude,
+                          value: this.state.longitude,
                           onChange: this.onChange,
                           required: true
                         }}
@@ -367,7 +390,7 @@ class editBox extends React.Component {
                         }}
                         inputProps={{
                           name: "descricao",
-                          value: this.state.caixa.descricao,
+                          value: this.state.descricao,
                           onChange: this.onChange,
                           required: true
                         }}
